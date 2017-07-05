17e5d306ebed7d29b4e74b742b25d02780c949ec
Hack Reactor Student <student@hackreactor.com>
2017-07-05 15:08:41 -0700

    Complete method definitions for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,25 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+
+    if (keys.length === 0) {
+      storage[0] = value;
+    } else {
+      storage[JSON.parse(keys[keys.length - 1]) + 1] = value; 
+    }
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var popValue = storage[keys[0]];
+    delete storage[keys[0]];
+    return popValue;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
