96de50b9abd3f627a070f11bbe26839b9b94655d
Hack Reactor Student <student@hackreactor.com>
2017-07-05 15:56:04 -0700

    completed functional-shared for stack and queue

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack['values'] = {};
+  extend(newStack, stackMethods);
+
+  return newStack;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var keys = Object.keys(this.values);
+  this.values[keys.length] = value; 
+};
 
+stackMethods.pop = function() {
+  var keys = Object.keys(this.values);
+  var popValue = this.values[keys.length - 1];
+  delete this.values[keys.length - 1];
+  return popValue;
+};
+
+stackMethods.size = function() {
+  var keys = Object.keys(this.values);
+  return keys.length;
+};
