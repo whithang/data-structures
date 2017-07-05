f61d080a49bd3b7fdb717d1040e82500f20f08e8
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:26:46 -0700

    Complete prototypal pattern for stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue['values'] = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  var keys = Object.keys(this.values);
 
+  if (keys.length === 0) {
+    this.values[0] = value;
+  } else {
+    this.values[JSON.parse(keys[keys.length - 1]) + 1] = value; 
+  }
+};
+
+queueMethods.dequeue = function() {
+  var keys = Object.keys(this.values);
+  var popValue = this.values[keys[0]];
+  delete this.values[keys[0]];
+  return popValue;
+};
+
+queueMethods.size = function() {
+  var keys = Object.keys(this.values);
+  return keys.length;
+
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj['values'] = {};
+  return obj;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(val) {
+  var keys = Object.keys(this.values);
+  this.values[keys.length] = val; 
+};
+
+stackMethods.pop = function() {
+  var keys = Object.keys(this.values);
+  var popValue = this.values[keys.length - 1];
+  delete this.values[keys.length - 1];
+  return popValue;
+};
+
+stackMethods.size = function() {
+  var keys = Object.keys(this.values);
+  return keys.length;
+};
 
 
