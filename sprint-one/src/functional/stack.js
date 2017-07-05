a024e9872720380b0a55b60f14ea9c3a96f81be1
Hack Reactor Student <student@hackreactor.com>
2017-07-05 15:13:54 -0700

    Implement enqueue, dequeue, and size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,23 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  
+
   someInstance.push = function(value) {
+    var keys = Object.keys(storage);
+    storage[keys.length] = value; 
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var popValue = storage[keys.length - 1];
+    delete storage[keys.length - 1];
+    return popValue;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
