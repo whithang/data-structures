d13d61ab5ebbeb8ee78b2ff19a45708055443b49
Hack Reactor Student <student@hackreactor.com>
2017-07-06 16:09:26 -0700

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,22 +2,64 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  extend(newTree, treeMethods);
+  newTree.children = []; 
 
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create the child = a new tree
+  //tree value = value
+  //tied to what created it with this
+  //add to this.children array
+  var newTree = new Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  var childrenTest;
+  var result = false;
+  if (arguments.length === 1) {
+    childrenTest = this.children;
+  } else {
+    childrenTest = arguments[1];
+  }
+
+  if (this.value === target) {
+    return true;
+  }
+  
+  if (childrenTest.length > 0) {
+    for (var i = 0; i < childrenTest.length; i++) {
+      
+      if (childrenTest[i].value === target) {
+        return true;
+      }
+      if (childrenTest[i].children.length > 0) {
+        result = childrenTest[i].contains(target, this.children[i].children);
+        if (result === true) {
+          return result;
+        }
+      }
+    }
+  }
+  return result;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  extend:
+  addChild:
+  contains:
  */
