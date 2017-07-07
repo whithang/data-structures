11ce8f3f69dc693a24a3a06f8749c37b2ae13970
Hack Reactor Student <student@hackreactor.com>
2017-07-07 10:26:40 -0700

    refactored tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -17,42 +17,23 @@ var extend = function(to, from) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  //create the child = a new tree
-  //tree value = value
-  //tied to what created it with this
-  //add to this.children array
-  var newTree = new Tree(value);
+  var newTree = Tree(value);
   this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
-  var childrenTest;
-  var result = false;
-  if (arguments.length === 1) {
-    childrenTest = this.children;
-  } else {
-    childrenTest = arguments[1];
-  }
 
   if (this.value === target) {
     return true;
   }
-  
-  if (childrenTest.length > 0) {
-    for (var i = 0; i < childrenTest.length; i++) {
-      
-      if (childrenTest[i].value === target) {
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
         return true;
       }
-      if (childrenTest[i].children.length > 0) {
-        result = childrenTest[i].contains(target, this.children[i].children);
-        if (result === true) {
-          return result;
-        }
-      }
     }
+    return false;
   }
-  return result;
 };
 
 
