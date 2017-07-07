17a92f700f46f27c0d35dcf3cacebb61960508a5
Hack Reactor Student <student@hackreactor.com>
2017-07-07 10:31:24 -0700

    refactored graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,16 +1,16 @@
  // Instantiate a new graph
 var Graph = function() {
- 
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this[node] = []; 
+  this.nodes[node] = []; 
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (var key in this) {
+  for (var key in this.nodes) {
     if (key === JSON.stringify(node)) {
       return true;
     }
@@ -20,24 +20,24 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  for (var key in this) {
+  for (var key in this.nodes) {
     if (key === JSON.stringify(node)) {
-      if (this[key].length > 0) {
-        for (var i = 0; i < this[key].length; i++) {
-          this.removeEdge(node, this[key][i]);
+      if (this.nodes[key].length > 0) {
+        for (var i = 0; i < this.nodes[key].length; i++) {
+          this.removeEdge(node, this.nodes[key][i]);
         }
       } 
-      delete this[key];
+      delete this.nodes[key];
     }
   }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  for (var key in this) {
-    if (key === JSON.stringify(fromNode) && this[key].length > 0) {
-      for (var i = 0; i < this[key].length; i++) {
-        if (this[key][i] === toNode) {
+  for (var key in this.nodes) {
+    if (key === JSON.stringify(fromNode) && this.nodes[key].length > 0) {
+      for (var i = 0; i < this.nodes[key].length; i++) {
+        if (this.nodes[key][i] === toNode) {
           return true;
         }
       }
@@ -48,30 +48,30 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  for (var key in this) {
+  for (var key in this.nodes) {
     if (key === JSON.stringify(fromNode)) {
-      this[key].push(toNode);
+      this.nodes[key].push(toNode);
     }
     if (key === JSON.stringify(toNode)) {
-      this[key].push(fromNode);
+      this.nodes[key].push(fromNode);
     }
   }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  for (var key in this) {
+  for (var key in this.nodes) {
     if (key === JSON.stringify(fromNode)) {
-      for (var i = 0; i < this[key].length; i++) {
-        if (this[key][i] === toNode) {
-          delete this[key][i];
+      for (var i = 0; i < this.nodes[key].length; i++) {
+        if (this.nodes[key][i] === toNode) {
+          delete this.nodes[key][i];
         }
       }
     }
     if (key === JSON.stringify(toNode)) {
-      for (var i = 0; i < this[key].length; i++) {
-        if (this[key][i] === fromNode) {
-          delete this[key][i];
+      for (var i = 0; i < this.nodes[key].length; i++) {
+        if (this.nodes[key][i] === fromNode) {
+          delete this.nodes[key][i];
         }
       }
     }
