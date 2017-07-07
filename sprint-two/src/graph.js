89582ff28963121ddc886f9d6d1f0af18347c469
Hack Reactor Student <student@hackreactor.com>
2017-07-06 17:49:00 -0700

    made progress on graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,12 +1,13 @@
  // Instantiate a new graph
 var Graph = function() {
   this.nodes = {};
-  this.edge = [];
+  //this.nodes.edge = [];
+  //need to refactor everything to match this new object formatting
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes.name = node; 
+  this.nodes.node = []; 
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -30,15 +31,41 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var key in this.nodes) {
+    if (this.nodes.name === fromNode) {
+      if (this.nodes.edge.includes(toNode)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  
+  for (var key in this.nodes) {
+    if (this.nodes.name === fromNode) {
+      this.nodes.edge.push(toNode);
+    }
+    if (this.nodes.name === toNode) {
+      this.nodes.edge.push(fromNode);
+    }
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var nodeIndex;
+  for (var key in this.nodes) {
+    if (this.nodes.name === fromNode) {
+      nodeIndex = this.nodes.edge.indexOf(toNode);
+      delete this.nodes.edge[nodeIndex];
+    }
+    if (this.nodes.name === toNode) {
+      nodeIndex = this.nodes.edge.indexOf(fromNode);
+      delete this.nodes.edge[nodeIndex];
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
