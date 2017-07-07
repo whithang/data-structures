b566124b76202e1d77ba38140c8c79d4e97b6694
Hack Reactor Student <student@hackreactor.com>
2017-07-07 10:59:32 -0700

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage.indexOf(item) !== -1) {
+    delete this._storage[this._storage.indexOf(item)];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add: O(n)
+  contains: O(n)
+  remove: O(n)
  */
