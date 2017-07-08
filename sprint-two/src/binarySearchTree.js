155efc7f1da6e460a45455c18ab8fc9ff234b3a5
Hack Reactor Student <student@hackreactor.com>
2017-07-08 09:55:57 -0700

    Complete binary tree and time complexity

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,24 +1,63 @@
 var BinarySearchTree = function(value) {
-  this.left = null;
-  this.right = null;
-  this.value = value;
+  var tree = Object.create(treePrototype);
+  tree.left = null;
+  tree.right = null;
+  tree.value = value;
+
+  return tree;
 };
 
-BinarySearchTree.prototype.insert = function(value) {
+var treePrototype = {};
+
+treePrototype.insert = function(value) {
   //places in the tree in the correct position
   
   //instantiate a new binarysearchtree
+  var newTree = BinarySearchTree(value);
     //if value is greater than this.value
-      //set this.right equal to new search tree
+      //if = null then
+        //set this.right equal to new search tree
+      // else recurse
     //else if value is less than this.value
-      //set this.left equal to new search tree
-     
+      //if = null then
+        //set this.left equal to new search tree
+      // else recurse
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = newTree;
+      return;
+    } else {
+      this.right.insert(value);
+    }
+  } else if (value < this.value) {
+    if (this.left === null) {
+      this.left = newTree;
+      return;
+    } else {
+      this.left.insert(value);
+    }
+  }  
 };
 
-BinarySearchTree.prototype.contains = function(value) {
+treePrototype.contains = function(value) {
   //returns a boolean
 
   //start with this and if equal to value
+  if (this.value === value) {
+    return true;
+  }
+
+  if (value > this.value) {
+    if (this.right !== null) {
+      return this.right.contains(value);
+    }
+  } else if (value < this.value) {
+    if (this.left !== null) {
+      return this.left.contains(value);
+    }
+  }
+  
+  return false;
     //return true
     //if value greater than, look to the right recursively   
     //is less than, look to the left recursively
@@ -26,15 +65,27 @@ BinarySearchTree.prototype.contains = function(value) {
   //return false;
 };
 
-BinarySearchTree.prototype.depthFirstLog = function(cb) {
+treePrototype.depthFirstLog = function(cb) {
   //execute cb on every value contained in tree
 
   //start with this and run cb on it
     //if value to left is not null, recursively run
     //if value to the right is not null, recursively run
+  cb(this.value);
+
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert: O(log n)
+  contains: O(log n)
+  depthFirstLog: O(n)
  */
